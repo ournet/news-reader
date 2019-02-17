88b370c636e0e57e844d8d28cec2ff4c326c1146
Mitica <canteadumitru@gmail.com>
2019-02-17 13:59:14 +0200

    fix get image lolor from webp

--- src/functions/image-color2.ts
+++ src/functions/image-color2.ts
@@ -1,6 +1,10 @@
-const getColors = require('get-image-colors')
+const getColors = require('get-image-colors');
+import sharp = require('sharp');
 
 export async function getImageColor(data: Buffer, contentType: string): Promise<string> {
+    if (contentType.endsWith('webp')) {
+        data = await sharp(data).toFormat('png').toBuffer();
+    }
     const colors = await getColors(data, contentType);
     return colors[0].hex().replace('#', '');
 }
