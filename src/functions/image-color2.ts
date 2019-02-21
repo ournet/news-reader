708bcd232c8a0a780a0c1bb4f5dae5ab08455326
Mitica <canteadumitru@gmail.com>
2019-02-21 09:18:59 +0200

    change image format on getting image color

--- src/functions/image-color2.ts
+++ src/functions/image-color2.ts
@@ -4,6 +4,7 @@ import sharp = require('sharp');
 export async function getImageColor(data: Buffer, contentType: string): Promise<string> {
     if (contentType.endsWith('webp')) {
         data = await sharp(data).toFormat('png').toBuffer();
+        contentType = 'image/png';
     }
     const colors = await getColors(data, contentType);
     return colors[0].hex().replace('#', '');
