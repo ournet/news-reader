9add3c16cd0c790a21a6f9daf96fb489812ff696
Dumitru C <canteadumitru@gmail.com>
2021-09-02 16:46:47 +0300

    removed indianexpress source

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
 import { parseLocale } from "./helpers";
 
 const locale = parseLocale(process.env.LOCALE || process.argv[1]) as Locale;
 
 if (!locale) {
-    throw new Error(`LOCALE is required!`);
+  throw new Error(`LOCALE is required!`);
 }
 
 import { logger } from "./logger";
@@ -16,36 +16,44 @@ import { Locale } from "./types";
 import { DbDataService } from "./services/data-service";
 
 async function start() {
-    const config = getConfigFromEnv();
-    const connection = await DbDataConnection.create(config.MONGO_DB_CONNECTION);
+  const config = getConfigFromEnv();
+  const connection = await DbDataConnection.create(config.MONGO_DB_CONNECTION);
 
-    const awsOptions = {
-        accessKeyId: config.AWS_ACCESS_KEY_ID,
-        secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
-        region: config.AWS_REGION,
-    };
+  const awsOptions = {
+    accessKeyId: config.AWS_ACCESS_KEY_ID,
+    secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
+    region: config.AWS_REGION
+  };
 
-    const dataService = new DbDataService(connection.mongoClient.db(), config.NEWS_ES_HOST, awsOptions);
+  const dataService = new DbDataService(
+    connection.mongoClient.db(),
+    config.NEWS_ES_HOST,
+    awsOptions
+  );
 
-    await dataService.init();
+  await dataService.init();
 
-    const newsReader = new NewsReader(config, dataService);
+  const newsReader = new NewsReader(config, dataService);
 
-    try {
-        await newsReader.start(locale);
-        await connection.close();
-    } catch (e) {
-        await connection.close();
-        throw e;
-    }
+  try {
+    await newsReader.start(locale);
+    await connection.close();
+  } catch (e) {
+    await connection.close();
+    throw e;
+  }
 }
 
 const startDate = Date.now();
 
 function getSeconds() {
-    return Math.round((Date.now() - startDate) / 1000);
+  return Math.round((Date.now() - startDate) / 1000);
 }
 
 start()
-    .catch(e => logger.warn(`ERROR ${locale.lang}-${locale.country}: ${e.message}`, e))
-    .then(() => logger.warn(`END ${locale.lang}-${locale.country} in ${getSeconds()}s`))
+  .catch((e) =>
+    logger.warn(`ERROR ${locale.lang}-${locale.country}: ${e.message}`, e)
+  )
+  .then(() =>
+    logger.warn(`END ${locale.lang}-${locale.country} in ${getSeconds()}s`)
+  );
--- src/process-feed.ts
+++ src/process-feed.ts
@@ -1,7 +1,11 @@
+const debug = require("debug")("ournet:news-reader");
 
-const debug = require('debug')('ournet:news-reader');
-
-import { readNewsFeed, NewsFeedItem, NewsSource, NewsFeed } from "./functions/read-news-feed";
+import {
+  readNewsFeed,
+  NewsFeedItem,
+  NewsSource,
+  NewsFeed
+} from "./functions/read-news-feed";
 import { logger } from "./logger";
 import { processFeedItem } from "./process-feed-item";
 import { NewsItem } from "@ournet/news-domain";
@@ -11,49 +15,67 @@ import { ImagesStorageService } from "./services/images-storage-service";
 import { TextTopicsService } from "./services/text-topics-service";
 
 export type ProcessFeedOptions = {
-    minDate: Date
-}
+  minDate: Date;
+};
 
-export async function processFeed(dataService: DataService, imagesStorage: ImagesStorageService,
-    topicsService: TextTopicsService, feed: NewsFeed, source: NewsSource, options: ProcessFeedOptions) {
-    let newsFeedItems: NewsFeedItem[];
+export async function processFeed(
+  dataService: DataService,
+  imagesStorage: ImagesStorageService,
+  topicsService: TextTopicsService,
+  feed: NewsFeed,
+  source: NewsSource,
+  options: ProcessFeedOptions
+) {
+  let newsFeedItems: NewsFeedItem[];
+  try {
+    newsFeedItems = await readNewsFeed(feed, source, options.minDate);
+  } catch (e) {
+    logger.error(`Error on reading news feed: ${feed.url}`, e);
+    return [];
+  }
+
+  if (!newsFeedItems.length) {
+    return [];
+  }
+
+  const newsItems: NewsItem[] = [];
+
+  for (const newsFeedItem of newsFeedItems) {
+    let newsItem: NewsItem | undefined;
+
     try {
-        newsFeedItems = await readNewsFeed(feed, source, options.minDate);
+      newsItem = await processFeedItem(
+        dataService,
+        imagesStorage,
+        topicsService,
+        newsFeedItem,
+        {
+          country: source.country,
+          lang: feed.language,
+          sourceId: source.id
+        }
+      );
     } catch (e) {
-        logger.error(`Error on reading news feed: ${feed.url}`, e);
-        return []
+      logger.error(
+        `error on process feed item: ${e.message}, ${newsFeedItem.link}`,
+        e
+      );
+      continue;
+    }
+    if (!newsItem) {
+      continue;
     }
 
-    if (!newsFeedItems.length) {
-        return []
-    }
+    newsItems.push(newsItem);
 
-    const newsItems: NewsItem[] = []
+    debug(`Saved news: ${newsItem.urlHost}${newsItem.urlPath}`);
+  }
 
-    for (const newsFeedItem of newsFeedItems) {
-        let newsItem: NewsItem | undefined;
+  await setLastReadedFeedUrl(
+    { lang: feed.language, country: source.country },
+    feed.url,
+    newsFeedItems[newsFeedItems.length - 1].link
+  );
 
-        try {
-            newsItem = await processFeedItem(dataService, imagesStorage, topicsService, newsFeedItem, {
-                country: source.country,
-                lang: feed.language,
-                sourceId: source.id,
-            });
-        } catch (e) {
-            logger.error(`error on process feed item: ${e.message}, ${newsFeedItem.link}`, e);
-            continue;
-        }
-        if (!newsItem) {
-            continue;
-        }
-
-        newsItems.push(newsItem);
-
-        debug(`Saved news: ${newsItem.urlHost}${newsItem.urlPath}`);
-    }
-
-    await setLastReadedFeedUrl({ lang: feed.language, country: source.country },
-        feed.url, newsFeedItems[newsFeedItems.length - 1].link);
-
-    return newsItems;
+  return newsItems;
 }
