d9277f2f2c662f0c6c8143f857170f3a78e8c4f8
Dumitru C <canteadumitru@gmail.com>
2022-11-30 10:52:40 +0200

    no loggly

--- src/logger.ts
+++ src/logger.ts
@@ -1,16 +1,17 @@
-
-export const logger: ILogger = require('ournet.logger');
-
-if (process.env.NODE_ENV === 'production') {
-    (<any>logger).loggly({
-        tags: ['news-reader'],
-        json: true
-    });
-    (<any>logger).removeConsole();
-}
-
 export interface ILogger {
-    error(message?: any, ...optionalParams: any[]): void
-    info(message?: any, ...optionalParams: any[]): void
-    warn(message?: any, ...optionalParams: any[]): void
+  error(message?: any, ...optionalParams: any[]): void;
+  info(message?: any, ...optionalParams: any[]): void;
+  warn(message?: any, ...optionalParams: any[]): void;
 }
+
+export const logger: ILogger = {
+  error: function (message?: any, ...optionalParams: any[]): void {
+    console.error(message, ...optionalParams);
+  },
+  info: function (message?: any, ...optionalParams: any[]): void {
+    console.info(message, ...optionalParams);
+  },
+  warn: function (message?: any, ...optionalParams: any[]): void {
+    console.warn(message, ...optionalParams);
+  }
+};
