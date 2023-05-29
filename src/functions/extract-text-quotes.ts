3b6e9583dcd3d1fdb8b5abf41e6a8b953ef19b0e
Dumitru C <canteadumitru@gmail.com>
2023-05-29 14:21:44 +0300

    Update extract-text-quotes.ts

--- src/functions/extract-text-quotes.ts
+++ src/functions/extract-text-quotes.ts
@@ -1,17 +1,25 @@
-
-const quoteParser = require('quote-parser');
+const quoteParser = require("quote-parser");
 
 export type TextQuote = {
-    index: number
-    text: string
-    author: { index: number, id: string }
-}
+  index: number;
+  text: string;
+  author: { index: number; id: string };
+};
 
-export function extractTextQuotes(text: string, lang: string, persons: { id: string, index: number }[]): TextQuote[] {
-    const quotes = quoteParser.parse(text, lang, { persons }) as TextQuote[];
-    if (!quotes || !quotes.length) {
-        return [];
-    }
+export function extractTextQuotes(
+  text: string,
+  lang: string,
+  persons: { id: string; index: number }[]
+): TextQuote[] {
+  let quotes: TextQuote[] = [];
+  try {
+    quotes = quoteParser.parse(text, lang, { persons }) as TextQuote[];
+  } catch (e) {
+    console.log(e);
+  }
+  if (!quotes || !quotes.length) {
+    return [];
+  }
 
-    return quotes.filter(item => (delete (<any>item).name) && !!item.author);
+  return quotes.filter((item) => delete (<any>item).name && !!item.author);
 }
